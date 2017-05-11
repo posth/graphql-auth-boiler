36e46df487c606cd1ad9f81f68737abf7e4bf084
posth <posth.marc@gmail.com>
2017-05-11 11:05:06 -0400

    Authentication to see if there is current user logged in added - graphql side

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,11 +1,22 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID } = graphql;
 
+const UserType = require('./user_type');
+
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     //By default, graphql expects at least one field available for all defined types
-    testField: { type: GraphQLID }
+    user: {
+      //everytime someone asks for a user we return the current user
+      type:  UserType,
+      //instruct graphQL how to return the current user
+      resolve(parentValue, args, req) {
+        //if the user is athentication, the req property will have the user property
+        //if there is no current user graphQL will return null to the front
+        return req.user;
+      }
+    }
   }
 });
 
