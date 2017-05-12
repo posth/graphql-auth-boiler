47aeb923a777d17afce2fe49726e9541edd5bcc3
posth <posth.marc@gmail.com>
2017-05-12 18:55:35 -0400

    React router setup with basic header and app root component

---
+++ client/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+
+//Functional compoment because all it does is show the header and child vue
+//No component level state so it's a functional component
+const App = (props) => {
+    return (
+        <div>
+            <Header />
+            {/*Always pass props to children just in case we get some*/}
+            {props.children}
+        </div>
+    );
+};
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -6,6 +6,11 @@ import ApolloClient from 'apollo-client';
 //react apollo exports multiple different properties, hence the {}
 import { ApolloProvider } from 'react-apollo';
 
+//Routing
+import { Router, hashHistory, Route, Indexroute } from 'react-router';
+
+import App from './components/App';
+
 const client = new ApolloClient({
   //options object for data ID from object function to be able to identify
   //every record that comes back from the server
@@ -20,9 +25,12 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>
-        Auth Starter
-      </div>
+      {/*Base router instance - hashhistory is used to not worry about proper setup in the express side of the app*/}
+      <Router history={hashHistory}>
+        {/*Base component is App*/}
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
