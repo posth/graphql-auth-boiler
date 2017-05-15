caa4d6b323f7c3b12add47675b5384ce4530077a
posth <posth.marc@gmail.com>
2017-05-15 10:49:14 -0400

    Logout functionality added to app - react side and graphql side

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 //Lets us get a query and a component to sandwhich them together when exporting them
 import { graphql } from 'react-apollo';
 
+//Queries
 import currentUserQuery from '../queries/CurrentUser';
+//Mutations
+import logoutMutation from '../mutations/Logout';
 
 //Class based component
 class Header extends Component {
+
+    onLogoutClick() {
+        //Calling a mutation
+        this.props.mutate({
+            //Rerun all the queries the mutation has affected, all components affected by new queries will change
+            refetchQueries: [{ query: currentUserQuery}]
+        });
+    }
 
     //Whether to show login or logout depending on auth
     renderButtons() {
@@ -14,16 +26,28 @@ class Header extends Component {
         const { loading, user } = this.props.data;
 
         //If the query hasn't completed yet to determine if user is authenticated or not
-        if(loading) {
+        if (loading) {
             return <div />
         }
 
         //if there is a currently authenticated user
-        if(user) {
-            return <div>Logout</div>;
+        if (user) {
+            // Using a <a> tag to get the same styling
+            return (
+                // Bind the context to this click event
+                <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>
+            );
         } else {
             return (
-                <div>Login</div>
+                // Two possible links that can show up - react wants one root dom element
+                <div>
+                    <li>
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </div>
             );
         }
     }
@@ -32,12 +56,20 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    {/*method to call to see which buttons to show*/}
-                    {this.renderButtons()}
+                    <Link to="/" className="brand-logo left">
+                        Home
+                    </Link>
+                    <ul className="right">
+                        {/*method to call to see which buttons to show*/}
+                        {this.renderButtons()}
+                    </ul>
                 </div>
             </nav>
         );
     }
 }
 
-export default graphql(currentUserQuery)(Header);
+//Multiple queries and mutations to a component, you have to stack up the graphql helper
+export default graphql(logoutMutation)(
+    graphql(currentUserQuery)(Header)
+);
