03fcd5a4dc12842b0398533718b9319f15231687
Marc Posth <posth.marc@gmail.com>
2017-06-04 12:57:39 -0400

    Geolocation added to google-map component + googlemaps api key attached to script on the index (few errors need to clear related to deprecation and google maps initMap usage)

--- client/components/google-map/BaseGoogleMap.js
+++ client/components/google-map/BaseGoogleMap.js
@@ -2,14 +2,47 @@ import React, { Component } from 'react';
 
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
 
+const geolocation = (
+    navigator.geolocation ?
+        navigator.geolocation :
+        ({
+            getCurrentPosition(success, failure) {
+                failure(`Your browser doesn't support geolocation.`);
+            }
+        })
+);
+
 const BasicMap = withGoogleMap(props => (
     <GoogleMap
-        defaultZoom={15}
-        defaultCenter={{ lat: 45.492160, lng: -73.635443 }}
+        zoom={props.zoom}
+        center={props.center}
     />
 ));
 
 export default class BaseGoogleMap extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            zoom: 1,
+            center: {lat: 0, lng: 0}
+        };
+    }
+
+    componentDidMount() {
+        geolocation.getCurrentPosition((position) => {
+
+            this.setState({
+                zoom: 15,
+                center: {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                }
+            });
+
+            // console.log(position.coords.latitude, position.coords.longitude);
+        })
+    }
 
     render() {
         return (
@@ -20,6 +53,8 @@ export default class BaseGoogleMap extends Component {
                 mapElement={
                     <div style={{ height: `100%` }} />
                 }
+                center={this.state.center}
+                zoom={this.state.zoom}
             />
         );
     }
